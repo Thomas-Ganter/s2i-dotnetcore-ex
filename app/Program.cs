6a382a32bd0f62c8713a0fa5902a6579f48122e5
Thomas Ganter <github.com@ganter.dyndns.org>
2018-08-24 12:23:30 +0200

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,6 +14,7 @@ namespace app
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("So, neuer Versuch");
             BuildWebHost(args).Run();
         }
 
